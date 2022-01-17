0cd9a65ed613980f59540c5752f82caae5b50e66
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-17 22:09:51 +0100

    with star

--- functions.test.js
+++ functions.test.js
@@ -1,44 +1,44 @@
 import { expect, test } from '@jest/globals';
-import { isPlainObject, makePairs, without, isEmpty, isEqualDeep, getUserName } from './functions.js';
+import * as functions from './functions.js';
 
 test('isPlainObject() is correctly implemented', () => {
-	expect(isPlainObject('aaaaaaaaa')).toBe(false);
-	expect(isPlainObject([])).toBe(false);
-	expect(isPlainObject(123)).toBe(false);
-	expect(isPlainObject({ name: 'ok' })).toBe(true);
-	expect(isPlainObject(false)).toBe(false);
-	expect(isPlainObject(true)).toBe(false);
-	expect(isPlainObject(undefined)).toBe(false);
-	expect(isPlainObject(null)).toBe(false);
+	expect(functions.isPlainObject('aaaaaaaaa')).toBe(false);
+	expect(functions.isPlainObject([])).toBe(false);
+	expect(functions.isPlainObject(123)).toBe(false);
+	expect(functions.isPlainObject({ name: 'ok' })).toBe(true);
+	expect(functions.isPlainObject(false)).toBe(false);
+	expect(functions.isPlainObject(true)).toBe(false);
+	expect(functions.isPlainObject(undefined)).toBe(false);
+	expect(functions.isPlainObject(null)).toBe(false);
 });
 
 test('makePairs() is correctly implemented', () => {
-	expect(makePairs({ a: 1, b: 2 })).toStrictEqual([['a', 1], ['b', 2]]);
-	expect(makePairs({ a: 1, ok: 2, name: 'Georg' })).toStrictEqual([['a', 1], ['ok', 2], ['name', 'Georg']]);
-	expect(makePairs({})).toStrictEqual([]);
-	expect(makePairs(null)).toStrictEqual([]);
-	expect(makePairs([])).toStrictEqual([]);
-	expect(makePairs([1, 2, 3])).toStrictEqual([]);
-	expect(makePairs(false)).toStrictEqual([]);
-	expect(makePairs(true)).toStrictEqual([]);
+	expect(functions.makePairs({ a: 1, b: 2 })).toStrictEqual([['a', 1], ['b', 2]]);
+	expect(functions.makePairs({ a: 1, ok: 2, name: 'Georg' })).toStrictEqual([['a', 1], ['ok', 2], ['name', 'Georg']]);
+	expect(functions.makePairs({})).toStrictEqual([]);
+	expect(functions.makePairs(null)).toStrictEqual([]);
+	expect(functions.makePairs([])).toStrictEqual([]);
+	expect(functions.makePairs([1, 2, 3])).toStrictEqual([]);
+	expect(functions.makePairs(false)).toStrictEqual([]);
+	expect(functions.makePairs(true)).toStrictEqual([]);
 });
 
 test('without() is correctly implemented', () => {
-	expect(without({ a: 1, b: 2 }, 'b')).toStrictEqual({ a: 1 });
-	expect(without({ a: 1, b: 2 }, 'a')).toStrictEqual({ b: 2 });
-	expect(without({ a: 1, b: 2 }, 'c')).toStrictEqual({ a: 1, b: 2 });
+	expect(functions.without({ a: 1, b: 2 }, 'b')).toStrictEqual({ a: 1 });
+	expect(functions.without({ a: 1, b: 2 }, 'a')).toStrictEqual({ b: 2 });
+	expect(functions.without({ a: 1, b: 2 }, 'c')).toStrictEqual({ a: 1, b: 2 });
 });
 
 test('isEmpty() is correctly implemented', () => {
-	expect(isEmpty({ a: 1, b: undefined })).toBe(false);
-	expect(isEmpty({})).toBe(true);
+	expect(functions.isEmpty({ a: 1, b: undefined })).toBe(false);
+	expect(functions.isEmpty({})).toBe(true);
 });
 
 test('isEqualDeep() is correctly implemented', () => {
-	expect(isEqualDeep({ a: 1, b: { c: 1 } }, { a: 1, b: { c: 1 } })).toBe(true);
-	expect(isEqualDeep({ a: 1, b: { c: 1 } }, { a: 1, b: { c: 2 } })).toBe(false);
+	expect(functions.isEqualDeep({ a: 1, b: { c: 1 } }, { a: 1, b: { c: 1 } })).toBe(true);
+	expect(functions.isEqualDeep({ a: 1, b: { c: 1 } }, { a: 1, b: { c: 2 } })).toBe(false);
 });
 
 it('matches if username has the correct prefix', () => {
-	expect(getUserName()).toEqual(expect.stringContaining('user_'));
+	expect(functions.getUserName()).toEqual(expect.stringContaining('user_'));
 });
